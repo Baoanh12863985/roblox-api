a23ee86356a34cfb0e5b165b0141d286fba49724
Baoanh12863985 <94727447+Baoanh12863985@users.noreply.github.com>
2024-12-21 17:03:42 +0700

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+let data = {
+    data_response: "print('excuted code')",
+};
+app.use("/", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+app.use(express.json());
+app.get("/api", (req, res) => {
+    res.send(JSON.stringify(data));
+});
+app.post("/api", (req, res) => {
+    data = req.body;
+    data.data_response = data.message;
+    res.send(JSON.stringify("Data received"));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
