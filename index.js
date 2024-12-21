3225051580609017395b5094eeead226f399cff2
Baoanh12863985 <94727447+Baoanh12863985@users.noreply.github.com>
2024-12-21 17:48:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 6000;
+
+let data = {
+    data_response: "print('excuted code')",
+};
+app.use("/", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+app.use(express.json());
+app.get("/api", (req, res) => {
+    res.send(JSON.stringify(data));
+});
+app.post("/api", (req, res) => {
+    data = req.body;
+    data.data_response = data.message;
+    res.send(JSON.stringify("Data received"));
+    setTimeout(function(){
+        data.data_response = ""
+    }, 1000)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
